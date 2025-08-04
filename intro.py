4167af125bada5bac26fd688f03174c8d4ee371a
Noha Shehab <nohashehab.iti@gmail.com>
2025-08-04 12:59:26 +0300

    introduction to python

---
+++ intro.py
@@ -0,0 +1,31 @@
+name = 'ahmed'
+print(name)
+
+# if = 10
+
+# print = "devops"
+# print("test")
+
+name='noha'
+
+stname = "My name is Hager, I lives in zagazig"
+
+stname2 = ('My name is Hager, '
+           'I lives in zagazig')
+
+print(stname2)
+
+
+stname3 = "My name is Hager,\nI lives in zagazig"
+print(stname3)
+
+
+# use """ or '''
+
+bio = """My name is Noha\nI works at ITI
+I lives in Mansoura
+I do love python"""
+
+print(bio)
+
+
