8c86d887c356c8445709ab6f06b50349567d680a
Noha Shehab <nohashehab.iti@gmail.com>
2025-08-07 10:56:30 +0300

    python tuple, dict, sets, ranges

---
+++ playground.py
@@ -0,0 +1,5 @@
+
+mys = {True, 34, 'new element', 'Information Technology Institute', 'noha',
+       'Ali', ('test', 10), 23.32, 'Ahmed'}
+print(mys)
+
---
+++ tips.py
@@ -0,0 +1,65 @@
+
+
+# program to simulate password entering at atm ?
+
+# enter password 3 times --> account locked ??
+"""
+login=False
+for i in range(3):
+    password = input("please enter password...")
+    if password=="abc":
+        login = True
+
+        print("login successfully")
+        break
+
+if i >=2 and login==False:
+    print("--- account is locked ?")
+
+"""
+
+
+
+
+
+# for i in range(3):
+#     password = input("please enter password : ")
+#     if password=="abc":
+#         print("-- login successfully")
+#         break
+# else:
+#     # this block will be executed ? only if loop reached its end
+#     print(" account is locked ")
+
+
+
+
+
+
+for i in range(10):
+    if i==4:
+        continue # skip to the next iteration
+    print(i)
+
+else:
+    print("======================")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
